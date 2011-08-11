65385f92340a2bc8f8c4b2e0cfe821247bc125dd
okame <pikkoro888@gmail.com>
2011-08-11 21:52:03 +0900

    delete client mock

--- server/pack.js
+++ server/pack.js
@@ -1,14 +1,19 @@
 var id = 0;
+var point = 0;
+var sumPoint = 0;
+var teamName = '';
+var item = 0;
+var status = 0;
+var x=1;
+var y=1;
 
-// TODO
-// Task division between pack and controller
 exports.Pack = function(con, x, y, v) {
   this.con = con;
-  this.x = x||0;
-  this.y = y||0;
-  this.v = v||1;
-  this.id = id++;
+  this.x = x||1;
+  this.y = y||1;
+  id++;
 }
+
 exports.Pack.prototype.getX = function() {
   return this.x;
 }
@@ -21,12 +26,15 @@ exports.Pack.prototype.getV = function() {
 exports.Pack.prototype.getId = function() {
   return this.id;
 }
-exports.Pack.prototype.next = function() {
-  this.send('next', '');
+exports.Pack.prototype.setId = function(id) {
+  this.id = id;
+}
+exports.Pack.prototype.next = function(arg) {
+  this.send('next', arg);
 }
 exports.Pack.prototype.move = function(msg) {
-  this.x = this.x + msg.i;
-  this.y = this.y + msg.j;
+  this.x = msg.i;
+  this.y = msg.j;
 }
 exports.Pack.prototype.send = function(ope, arg) {
   this.con.send(this.createMsg(ope, arg));
@@ -35,10 +43,17 @@ exports.Pack.prototype.render = function(stage) {
   this.send('render', stage);
 }
 exports.Pack.prototype.createMsg = function(ope, arg) {
-  var msg = {
-    ope:ope,
-    arg:arg
-  };
-  return JSON.stringify(msg);
+	var msg = {
+		ope:ope,
+		arg:arg
+	};
+	return JSON.stringify(msg);
+}
+exports.Pack.prototype.createSendPack = function(msg) {
+	var pack = {};
+	pack.x = this.x;
+	pack.y = this.y;
+	pack.id = this.id;
+	return pack;
 }
 
