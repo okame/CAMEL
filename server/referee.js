03165e6e31d150de5a67cb6069b3bb756f69547d
takagi@home <takagi@okame-mac.(none)>
2011-08-17 21:46:29 +0900

    create referee, which calcurate point and judge walking, and stage, which stage object

---
+++ server/referee.js
@@ -0,0 +1,30 @@
+/**
+ * referee Object
+ *
+ * referee{type : Object}
+ */
+var referee = {}
+
+/* Load library */
+var sys = require('sys');
+var env = require('./env').env;
+
+/* 
+ * Member variables
+ */
+
+/* 
+ * Methods
+ */
+
+/**
+ * Initialize the referee object
+ */
+referee.init = function() {
+}
+
+referee.checkNextCell = function(){
+}
+
+
+exports.referee = referee;
